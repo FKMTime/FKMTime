01474d4eb5bcc32767b80abb6967ca9700bf47ee
Maksymilian Gala <maksymilian.gala1@gmail.com>
2024-05-02 20:44:30 +0200

    docs: boostrap docusaurus project

---
+++ docs/docusaurus.config.ts
@@ -0,0 +1,61 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'FKMTime',
+  tagline: 'A system for running speedcubing competitions without scorecards!',
+  favicon: 'img/favicon.ico',
+  url: 'https://maxidragon.github.io',
+  baseUrl: '/FKMTime/',
+  organizationName: 'maxidragon',
+  projectName: 'FKMTime',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+  plugins: [require.resolve('docusaurus-lunr-search')],
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          editUrl:
+            'https://github.com/maxidragon/FKMTime/tree/main/docs/',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'FKMTime',
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Documentation',
+        },
+        {
+          href: 'https://github.com/maxidragon/FKMTime',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
