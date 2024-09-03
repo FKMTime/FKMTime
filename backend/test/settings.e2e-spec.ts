f724f0d40e03a64bcbe54a2b56817b08cb93a6cc
Maksymilian Gala <maksymilian.gala1@gmail.com>
2024-09-03 17:47:31 +0200

    test(e2e): quick actions test

---
+++ backend/test/settings.e2e-spec.ts
@@ -0,0 +1,79 @@
+import { INestApplication } from '@nestjs/common';
+import { TestingModule, Test } from '@nestjs/testing';
+import { PrismaClient } from '@prisma/client';
+import { AppModule } from 'src/app.module';
+import * as request from 'supertest';
+import { loginWithFKMAccount } from './helpers/auth.helper';
+
+describe('SettingsController (e2e)', () => {
+  let app: INestApplication;
+  let prismaClient: PrismaClient;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule, PrismaClient],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+    prismaClient = moduleFixture.get<PrismaClient>(PrismaClient);
+  }, 30000);
+
+  afterAll(async () => {
+    await app.close();
+    await prismaClient.$disconnect();
+  }, 30000);
+
+  it('returns settings', async () => {
+    const user = await loginWithFKMAccount(app, {
+      username: 'admin',
+      password: 'admin',
+    });
+
+    await request(app.getHttpServer())
+      .get('/settings')
+      .set('Authorization', `Bearer ${user.token}`)
+      .expect(200);
+  });
+
+  it('creates a quick action', async () => {
+    const user = await loginWithFKMAccount(app, {
+      username: 'admin',
+      password: 'admin',
+    });
+
+    await request(app.getHttpServer())
+      .post('/settings/quick-actions')
+      .set('Authorization', `Bearer ${user.token}`)
+      .send({
+        name: 'Test',
+        comment: 'Test',
+        giveExtra: true,
+      })
+      .expect(201);
+  });
+
+  it('get quick actions', async () => {
+    const user = await loginWithFKMAccount(app, {
+      username: 'admin',
+      password: 'admin',
+    });
+
+    const response = await request(app.getHttpServer())
+      .get('/settings/quick-actions')
+      .set('Authorization', `Bearer ${user.token}`)
+      .expect(200);
+
+    expect(response.body).toEqual([
+      {
+        id: expect.any(String),
+        createdAt: expect.any(String),
+        updatedAt: expect.any(String),
+        userId: expect.any(String),
+        name: 'Test',
+        comment: 'Test',
+        giveExtra: true,
+      },
+    ]);
+  });
+});
