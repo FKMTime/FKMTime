f329309aa28482c5df67ba8cc50196b6bba08f8d
Maksymilian Gala <maksymilian.gala1@gmail.com>
2025-01-17 09:39:38 +0100

    fix(frontend): styling

--- frontend/src/Components/ui/card.tsx
+++ frontend/src/Components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
     <div
         ref={ref}
         className={cn(
-            "rounded-xl border bg-card text-card-foreground shadow-xl dark:shadow-lg dark:shadow-secondary",
+            "rounded-xl border bg-card text-card-foreground shadow-xl dark:shadow-md dark:shadow-secondary",
             className
         )}
         {...props}
