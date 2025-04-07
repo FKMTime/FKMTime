a9904b7aec7b8fa004e8745d2602da5edd2323b3
Maksymilian Gala <maksymilian.gala1@gmail.com>
2025-04-07 14:20:17 +0200

    fix: delay chart (closes #242)

--- frontend/src/Components/CompetitionStatistics/Charts/DelayChart.tsx
+++ frontend/src/Components/CompetitionStatistics/Charts/DelayChart.tsx
@@ -1,3 +1,4 @@
+import { useMemo, useState } from "react";
 import { CartesianGrid, Line, LineChart, XAxis, YAxis } from "recharts";
 
 import {
@@ -27,59 +28,89 @@ interface DelayChartData {
 }
 
 const DelayChart = ({ data }: DelayChartData) => {
+    const [longestTick, setLongestTick] = useState("");
+
+    const tickFormatter = (val: number) => {
+        const formattedTick = `${val} min`;
+        if (longestTick.length < formattedTick.length) {
+            setLongestTick(formattedTick);
+        }
+        return formattedTick;
+    };
+
+    const getYAxisTickLen = () => {
+        const charWidth = 8;
+        return longestTick.length * charWidth;
+    };
+
+    const maxDelay = useMemo(() => {
+        return Math.max(
+            ...data.roundsStatistics.map((r) => r.delayInMinutes || 0)
+        );
+    }, [data]);
+
+    const yTicks = useMemo(() => {
+        const roundedMax = Math.ceil(maxDelay / 5) * 5;
+        const ticks = [];
+        for (let i = 0; i <= roundedMax; i += 5) {
+            ticks.push(i);
+        }
+        return ticks;
+    }, [maxDelay]);
+
     return (
-        <>
-            <Card>
-                <CardHeader>
-                    <CardTitle>
-                        Delay for {new Date(data.date).toLocaleDateString()}
-                    </CardTitle>
-                    <CardDescription>
-                        Scheduled start time and time of the first solve are
-                        compared
-                    </CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <ChartContainer
-                        config={chartConfig}
-                        className="h-64 w-full"
+        <Card>
+            <CardHeader>
+                <CardTitle>
+                    Delay for {new Date(data.date).toLocaleDateString()}
+                </CardTitle>
+                <CardDescription>
+                    Scheduled start time and time of the first solve are
+                    compared
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <ChartContainer config={chartConfig}>
+                    <LineChart
+                        accessibilityLayer
+                        data={data.roundsStatistics}
+                        height={200}
+                        margin={{
+                            left: 12,
+                            right: 12,
+                            top: 10,
+                            bottom: 10,
+                        }}
                     >
-                        <LineChart
-                            accessibilityLayer
-                            data={data.roundsStatistics}
-                            margin={{
-                                left: 12,
-                                right: 12,
-                            }}
-                        >
-                            <CartesianGrid vertical={false} />
-                            <YAxis
-                                axisLine={false}
-                                tickLine={false}
-                                tickMargin={8}
-                                tickFormatter={(value) => `${value} min`}
-                            />
-                            <XAxis
-                                dataKey="roundName"
-                                tickLine={false}
-                                axisLine={false}
-                                tickFormatter={(value) => value}
-                            />
-                            <ChartTooltip
-                                cursor={false}
-                                content={<ChartTooltipContent hideLabel />}
-                            />
-                            <Line
-                                dataKey="delayInMinutes"
-                                type="natural"
-                                strokeWidth={2}
-                                dot={false}
-                            />
-                        </LineChart>
-                    </ChartContainer>
-                </CardContent>
-            </Card>
-        </>
+                        <CartesianGrid vertical={false} />
+                        <YAxis
+                            axisLine={false}
+                            tickLine={false}
+                            tickMargin={8}
+                            domain={[0, "auto"]}
+                            tickFormatter={tickFormatter}
+                            width={getYAxisTickLen()}
+                            ticks={yTicks}
+                        />
+                        <XAxis
+                            dataKey="roundName"
+                            tickLine={false}
+                            axisLine={false}
+                        />
+                        <ChartTooltip
+                            cursor={false}
+                            content={<ChartTooltipContent hideLabel />}
+                        />
+                        <Line
+                            dataKey="delayInMinutes"
+                            type="natural"
+                            strokeWidth={2}
+                            dot={false}
+                        />
+                    </LineChart>
+                </ChartContainer>
+            </CardContent>
+        </Card>
     );
 };
 
